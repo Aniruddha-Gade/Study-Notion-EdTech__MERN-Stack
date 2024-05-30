fe182fdf284dc394f34d17b09c5db20f4ccdc527
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-30 23:20:47 +0530

    update: reset password controller

--- backend/controllers/resetPassword.js
+++ backend/controllers/resetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async (req, res) => {
 
 
         // create url
-        const url = `http://localhost:5173/update-password/${token}`;
+        const url = `https://study-notion-mern-stack.netlify.app/update-password/${token}`;
 
         // send email containing url
         await mailSender(email, 'Password Reset Link', `Password Reset Link : ${url}`);
