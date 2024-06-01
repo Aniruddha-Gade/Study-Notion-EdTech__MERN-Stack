8590fa6ad3063586ec189e737449c4dfa0dadde3
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-06-01 09:16:10 +0530

    update style of custom button for disabled case

--- frontend/src/components/common/IconBtn.jsx
+++ frontend/src/components/common/IconBtn.jsx
@@ -4,8 +4,8 @@ export default function IconBtn({ text, onclick, children, disabled, outline = f
             disabled={disabled}
             onClick={onclick}
             className={`flex items-center justify-center outline-none ${outline ? "border border-yellow-50 bg-transparent" : "bg-yellow-50"
-                } cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 hover:bg-black hover:text-yellow-50 duration-300 ${customClasses}
-                ${disabled && 'cursor-not-allowed'} `}
+                } gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 hover:bg-black hover:text-yellow-50 duration-300 ${customClasses}
+                ${disabled && 'cursor-not-allowed hover:bg-yellow-50 hover:text-black'} `}
             type={type}
         >
             {
